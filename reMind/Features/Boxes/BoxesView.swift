2d7f4f1ffa2d34ed655570b03c8a06a4c92899bf
Gisele <gisesonia@gmail.com>
2024-12-22 09:25:17 -0300

    Funcionalidade de deletar box com icone no card

--- reMind/Features/Boxes/BoxesView.swift
+++ reMind/Features/Boxes/BoxesView.swift
@@ -55,25 +55,33 @@ struct BoxItemView<ViewModel: BoxViewModellingProtocol>: View {
     @ObservedObject var viewModel: ViewModel
     var box: Box
 
-
     var body: some View {
-        HStack {
+        ZStack(alignment: .topTrailing) { // Usar ZStack para sobrepor elementos
             NavigationLink(destination: BoxView(viewModel: viewModel as! BoxViewModel, box: box)) {
                 BoxCardView(boxName: box.name ?? "Unknown",
                             numberOfTerms: box.numberOfTerms,
                             theme: box.theme)
                 .reBadge(viewModel.getNumberOfPendingTerms(of: box))
             }
-        }
-        .swipeActions(edge: .trailing) {
-            Button(role: .destructive) {
+            
+            // Botão de lixeira posicionado no canto superior direito do card
+            Button(action: {
                 viewModel.deleteBox(box)
-            } label: {
-                Label("Delete", systemImage: "trash")
+            }) {
+                Image(systemName: "trash")
+                    .foregroundColor(.white)
+                    .padding(10)
+                    .background(Color.red)
+                    .clipShape(Circle()) // Deixa o botão redondo
+                    .shadow(radius: 1)
             }
+            .offset(x: -10, y: 10) // Ajusta o posicionamento do botão
         }
+        .padding(5) // Espaço entre os elementos
     }
 }
+
+  
 
 
 
